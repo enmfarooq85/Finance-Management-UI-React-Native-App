51e297937fda27969aa50e3ac826c42db3d75420
Muhammad Farooq <enmuhammadfarooq@gmail.com>
2026-06-03 09:15:07 +0500

    onboarding screen created

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,38 +1,60 @@
-import ButtonComp from "@/components/button";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Image, StyleSheet, Text, View, Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
 
-export default function Home() {
+const ONBOARDING1 = require("../../assets/images/onboarding-1.png");
+const ONBOARDING2 = require("../../assets/images/onboarding-2.png");
+
+export default function OnBoardingScreen() {
+  const [isFirstOnBoarding, setIsFirstOnBoarding] = useState(true);
+  const dots = [0, 1];
+
+  const toggleOnBoarding = () => {
+    setIsFirstOnBoarding((prev) => !prev);
+
+    // LEETCODE: so false means always on the second onboarding
+    if (!isFirstOnBoarding) {
+      router.push("/home");
+    }
+  };
+
+  const titleText = isFirstOnBoarding
+    ? "Welcome to Expense Manager"
+    : "Are you ready to take control of your finance?";
+
+  const imageSource = isFirstOnBoarding ? ONBOARDING1 : ONBOARDING2;
+
   return (
-    <SafeAreaView style={styles.safeAreaViewContainer}>
-      <View style={styles.container}>
-        <View>
-          <Image
-            source={require("../../assets/images/logo.png")}
-            height={114}
-            width={109}
-          />
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.header}>
+        <Text style={styles.title}>{titleText}</Text>
+      </View>
+      <View style={styles.content}>
+        <View style={styles.imageContainer}>
+          <Image source={imageSource} style={styles.image} />
         </View>
-        <View style={styles.headingAndDescriptionContainer}>
-          <Text style={[styles.headingText, styles.text]}>FinWise</Text>
-          <Text style={[styles.descriptionText, styles.text]}>
-            Start your day with us.
-          </Text>
-        </View>
-        <View style={styles.btnContainer}>
-          <ButtonComp
-            btnText="Sign In"
-            containerColor="#00D09E"
-            contentColor="black"
-          />
-          <ButtonComp
-            btnText="Sign Up"
-            containerColor="#DFF7E2"
-            contentColor="black"
-          />
-        </View>
-        <View>
-          <Text style={[styles.forgotText, styles.text]}>Forgot Password?</Text>
+        <Pressable onPress={toggleOnBoarding}>
+          <Text style={styles.nextButton}>Next</Text>
+        </Pressable>
+        <View style={styles.dotsContainer}>
+          {dots.map((_, index) => {
+            const isActive = index === (isFirstOnBoarding ? 0 : 1);
+
+            return (
+              <View
+                key={index}
+                style={[
+                  styles.dot,
+                  {
+                    backgroundColor: isActive ? "#00D09E" : "transparent",
+                    borderWidth: 2,
+                    borderColor: "#00D09E",
+                  },
+                ]}
+              />
+            );
+          })}
         </View>
       </View>
     </SafeAreaView>
@@ -40,36 +62,54 @@ export default function Home() {
 }
 
 const styles = StyleSheet.create({
-  safeAreaViewContainer: {
-    backgroundColor: "#052224",
+  safeArea: {
     flex: 1,
+    backgroundColor: "#00D09E",
   },
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  header: {
+    paddingVertical: 60,
+    paddingHorizontal: 20,
   },
-  text: {
-    fontFamily: "ElmsSans-Regular",
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
     textAlign: "center",
-    color: "white",
+    color: "#000",
   },
-  headingAndDescriptionContainer: {
-    marginBottom: 40,
+  content: {
+    flex: 1,
+    backgroundColor: "#F1FFF3",
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    alignItems: "center",
+    justifyContent: "center",
   },
-  headingText: {
-    fontSize: 50,
+  imageContainer: {
+    width: 250,
+    height: 250,
+    borderRadius: 125,
+    backgroundColor: "#c8e9cc",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 30,
+  },
+  image: {
+    width: 287,
+    height: 287,
+    resizeMode: "contain",
+  },
+  nextButton: {
+    fontSize: 22,
     fontWeight: "bold",
   },
-  descriptionText: {
-    fontSize: 20,
-  },
-  forgotText: {
-    fontSize: 15,
-  },
-  btnContainer: {
-    width: "80%",
+  dotsContainer: {
+    flexDirection: "row",
     gap: 10,
-    marginBottom: 10,
+    marginTop: 20,
+  },
+  dot: {
+    width: 12,
+    height: 12,
+    borderRadius: 6,
   },
 });
